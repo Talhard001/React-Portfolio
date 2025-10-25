d53ea6d996ea7866da61e26f5bc0242ddbb7795f
Talha <talharrd5994@example.com>
2025-10-26 04:04:22 +0500

    e

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 export const submitContact = async (formData) => {
   const response = await fetch(
-    "https://react-portfolio-production-87de.up.railway.app/api/contact", // ✅ same domain as backend
+    
     {
       method: "POST",
       headers: {
